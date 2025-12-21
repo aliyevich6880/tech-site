59b58a094ceeb544f47ec9ffcaa64e90924b642d
aliyevich6880 <toychiyevsheroz821@gmail.com>
2025-12-21 08:19:02 +0500

    Footerga o'zgartirish kiritildi

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -190,8 +190,8 @@
                   d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                 ></path>
               </svg>
-              <a href="mailto:info@techcollege.uz" class="hover:underline"
-                >info@techcollege.uz</a
+              <a href="mailto:pt2texnikum@gmail.com" class="hover:underline"
+                >pt2texnikum@gmail.com</a
               >
             </li>
           </ul>
